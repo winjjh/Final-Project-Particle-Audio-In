9c2fc00bd3a51232a50a574bf569bca39992ebdd
JongHee <jungj566@newschool.edu>
2022-04-26 19:57:30 -0400

    Initial commit

---
+++ particle.js
@@ -0,0 +1,17 @@
+let mic;
+
+function setup() {
+  createCanvas(1700, 1700);
+  // create and start the audio
+  mic = new p5.AudioIn();
+  mic.start();
+}
+  
+function draw(){
+  stroke(random(500),random(240),random(270))
+  fill(random(255),random(255),random(255), 30)
+  strokeWeight(random (1200))
+  
+  let diameter = mic.getLevel()*2000
+  circle(random(width),random(height),diameter)
+}
